261edb7f90a2c7d503be600b5eb437c8312fdc73
zooyaho <jiwoo1268@naver.com>
2024-11-03 15:19:15 +0900

    style: reset css 추가 및 module css 적용을 위한 타입 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // port: 3000, // 원하는 포트 번호로 변경 (예: 3000)
+  },
+});
