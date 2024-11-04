df1d71832be38b52cf540c79d6ad67703b8dbd87
zooyaho <jiwoo1268@naver.com>
2024-11-04 15:13:43 +0900

    feat: 로그인, 회원가입 요청 적용
    - 로그인 성공 시 결과의 토큰값을 localstorage에 저장 후 루트 경로로 이동
    - 회원가입 성공 시 로그인 페이지로 이동

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,38 @@
 import AuthForm from "@/components/auth/AuthForm";
 import styles from "./LoginPage.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { postLogin } from "@/api/authApi";
+import { useEffect } from "react";
 
 export default function LoginPage() {
-  const onLoginSubmit = (id: string, pw: string) => {
-    console.log(id, pw);
+  const navigate = useNavigate();
+
+  /**
+   * 로그인 이벤트 핸들러
+   * - 로그인 요청
+   * > 성공 시 localstorage에 token 저장 후 루트 경로로 이동
+   * > 실패 시 alert 활성화
+   */
+  const onLoginSubmit = async (id: string, pw: string) => {
+    try {
+      const result = await postLogin(id, pw);
+      localStorage.setItem("token", result.token);
+      alert(result.message);
+      navigate("/");
+    } catch (e) {
+      alert("로그인 실패. 다시 시도해주세요.");
+    }
   };
+
+  // 컴포넌트가 렌더링될 때 토큰 확인하여 리다이렉트
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // 토큰이 있으면 루트경로로 리다이렉트
+      navigate("/");
+    }
+  }, [navigate]);
 
   return (
     <section className={styles.section}>
--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -1,10 +1,27 @@
 import AuthForm from "@/components/auth/AuthForm";
 import styles from "./SignupPage.module.css";
+import { postSignUp } from "@/api/authApi";
+import { useNavigate } from "react-router-dom";
 
 export default function SingupPage() {
-  const onSignupSubmit = (id: string, pw: string) => {
-    console.log(id, pw);
+  const navigate = useNavigate();
+
+  /**
+   * 회원가입 이벤트 핸들러
+   * - 회원가입 요청
+   * > 성공 시 로그인 페이지로 이동
+   * > 실패 시 alert 활성화
+   */
+  const onSignupSubmit = async (id: string, pw: string) => {
+    try {
+      const result = await postSignUp(id, pw);
+      alert(result.message);
+      navigate("/auth/login");
+    } catch (e) {
+      alert("회원가입 실패. 다시 시도해주세요.");
+    }
   };
+
   return (
     <section className={styles.section}>
       <h1 className={styles.title}>회원가입</h1>
