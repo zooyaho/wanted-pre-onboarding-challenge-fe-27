9a2bd91b5b7c5b4b4d8616229666002f8fdcad66
zooyaho <jiwoo1268@naver.com>
2024-11-04 23:30:49 +0900

    feat: todo 목록 section 퍼블리싱 작업, api 데이터 연결, 항목 클릭 시 경로 변경 적용

--- src/components/todo/TodoListSection/TodoListSection.tsx
+++ src/components/todo/TodoListSection/TodoListSection.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./TodoListSection.module.css";
 import Button from "@/components/common/Button";
+import { TodoListType } from "@/types/todo.type";
+import { formatToYYYYMMDD } from "@/utils/formatDate";
+
+interface TodoListSectionPropsType {
+  todos: TodoListType;
+}
 
 /** todo 목록 section */
-export default function TodoListSection() {
+export default function TodoListSection({ todos }: TodoListSectionPropsType) {
   return (
     <section className={styles.section}>
       <header className={styles.header}>
@@ -12,7 +18,19 @@ export default function TodoListSection() {
         </Link>
       </header>
 
-      <div className={styles["todo-list-container"]}>TodoList</div>
+      <ul className={styles["todo-list"]}>
+        {todos &&
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <Link to={`/${todo.id}`} className={`${styles["todo-item"]}`}>
+                <p className={styles["todo-title"]}>{todo.title}</p>
+                <span className={styles["todo-last-update"]}>
+                  {formatToYYYYMMDD(todo.updatedAt)}
+                </span>
+              </Link>
+            </li>
+          ))}
+      </ul>
     </section>
   );
 }
--- src/pages/TodoPage/TodoPage.tsx
+++ src/pages/TodoPage/TodoPage.tsx
@@ -1,14 +1,30 @@
 import RootLayout from "@/components/layout/RootLayout";
 import styles from "./TodoPage.module.css";
 import TodoListSection from "@/components/todo/TodoListSection";
+import { useEffect, useState } from "react";
+import { getTodos } from "@/api/todoApi";
+import { TodoListType } from "@/types/todo.type";
 
 export default function TodoPage() {
+  const [todos, setTodos] = useState<TodoListType>([]);
+
+  useEffect(() => {
+    // 초기 렌더링 시 todos 초기화
+    const fetchGetTodos = async () => {
+      const todos = await getTodos();
+      setTodos(todos.data);
+    };
+    fetchGetTodos();
+
+    // TODO :: 경로 변경 시 todos 업데이트
+  }, []);
+
   return (
     <RootLayout mainStyle={{ gap: "20px" }}>
       {/* todo 목록 */}
-      <TodoListSection />
+      <TodoListSection todos={todos} />
       {/* todo 상세 */}
-      <TodoListSection />
+      <TodoListSection todos={todos} />
     </RootLayout>
   );
 }
