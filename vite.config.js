132a9b4117b0e9c3928676f425cd5518d3228b35
zooyaho <jiwoo1268@naver.com>
2024-11-04 13:20:24 +0900

    chore: vite-tsconfig-paths 설치하여 path alias 설정
    - vite config의 plugins설정 추가하여 ts config에 설성한 path를 vite에도 적용

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     // port: 3000, // 원하는 포트 번호로 변경 (예: 3000)
   },
