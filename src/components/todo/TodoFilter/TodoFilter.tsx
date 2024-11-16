33a6991a24b373bbd9c53018b8232923ef7ee910
zooyaho <jiwoo1268@naver.com>
2024-11-16 22:10:31 +0900

    feat: 정렬 드롭다운 선택 시 현재 쿼리 스트링 변경 로직 추가

--- src/components/todo/TodoFilter/TodoFilter.tsx
+++ src/components/todo/TodoFilter/TodoFilter.tsx
@@ -6,16 +6,50 @@ import {
   PriorityOptionValueType,
   SortOptionValueType,
 } from "./TodoFilter.type";
+import { useSearchParams } from "react-router-dom";
 
 export default function TodoFilter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   /* 정렬 드롭다운 선택 메서드 */
   const onSelectSortDropdown = (value: SortOptionValueType | null) => {
     console.log("정렬 드롭다운 >> ", value);
+
+    const newSearchParams = new URLSearchParams(searchParams); // 기존 값 유지
+
+    if (value) {
+      Object.entries(value).forEach(([key, value]) => {
+        newSearchParams.set(key, value.toString());
+      });
+      console.log("[정렬] newSearchParams >> ", newSearchParams.toString());
+    } else {
+      newSearchParams.delete("order");
+      newSearchParams.delete("sort");
+    }
+
+    setSearchParams(newSearchParams, {
+      replace: true,
+    });
   };
 
   /* 우선순위 드롭다운 선택 메서드 */
   const onSelectPriorityDropdown = (value: PriorityOptionValueType | null) => {
     console.log("우선순위 >> ", value);
+
+    const newSearchParams = new URLSearchParams(searchParams); // 기존 값 유지
+
+    if (value) {
+      Object.entries(value).forEach(([key, value]) => {
+        newSearchParams.set(key, value.toString());
+      });
+      console.log("[우선순위] newSearchParams >> ", newSearchParams.toString());
+    } else {
+      newSearchParams.delete("priority");
+    }
+
+    setSearchParams(newSearchParams, {
+      replace: true,
+    });
   };
 
   /* 검색 인풋 */
@@ -24,10 +58,22 @@ export default function TodoFilter() {
   ) => {
     if (event.key === "Enter") {
       const keyword = (event.target as HTMLInputElement).value.trim();
+
+      const newSearchParams = new URLSearchParams(searchParams); // 기존 값 유지
+
       if (keyword) {
-        console.log("keyword >> ", keyword);
-        // navigate(`?keyword=${encodeURIComponent(keyword)}`);
+        newSearchParams.set("keyword", keyword.toString());
+        console.log(
+          "[우선순위] newSearchParams >> ",
+          newSearchParams.toString()
+        );
+      } else {
+        newSearchParams.delete("keyword");
       }
+
+      setSearchParams(newSearchParams, {
+        replace: true,
+      });
     }
   };
 
